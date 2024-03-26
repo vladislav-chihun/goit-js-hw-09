42c7e87e4799f3e77bbae178f48ff7d4940df5da
vladislav-chihun <vladikkosmo@gmail.com>
2024-03-26 21:56:02 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig(({ command }) => {
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
-    root: 'src',
+    root: 'src/public',
     build: {
       sourcemap: true,
 
