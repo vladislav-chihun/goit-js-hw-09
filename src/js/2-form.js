7307506f75b0e3262525a4d9536d2f093b45cbff
vladislav-chihun <vladikkosmo@gmail.com>
2024-03-26 21:37:43 +0100

    а

---
+++ src/js/2-form.js
@@ -0,0 +1,31 @@
+const form = document.querySelector("form")
+const textarea = document.querySelector("textarea")
+const storageKey = "feedback-form-state"
+form.addEventListener("input", formChange)
+function formChange(e) {
+    const message = textarea.value.trim();
+    const email = form.elements.email.value.trim();
+    const data = JSON.stringify({ email, message });
+    localStorage.setItem(storageKey, data)
+}
+
+form.addEventListener("submit", formSubmit);
+function formSubmit(e) {
+    e.preventDefault();
+    const message = textarea.value.trim();
+    const email = form.elements.email.value.trim();
+    if (message === "" || email === "") return;
+    console.log({ email, message });
+    localStorage.removeItem(storageKey);
+    textarea.value = "";
+    form.elements.email.value = "";
+}
+const storageData = localStorage.getItem(storageKey)?? "";
+
+try {
+    const parseData = JSON.parse(storageData);
+    textarea.value = parseData.message;
+    form.elements.email.value = parseData.email;
+} catch {
+    console.log("No Data")
+}
